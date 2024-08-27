c506c420a926495236545d257a26be0b95cce9f5
gartz <gartz@google.com>
2024-08-27 17:07:46 +0000

    Add base mui layout with upload button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider, CssBaseline } from '@mui/material'; 
+import theme from './theme';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children} 
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ app/theme.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  // Your custom theme options here
+});
+
+export default theme;
