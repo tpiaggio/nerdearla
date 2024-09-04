c81de14709cc24c1e55cedbbea3e7ce02888dac3
gartz-google <gartz@google.com>
2024-09-04 16:55:36 -0700

    Upload with image compression to integrate with Gemini API (#2)
    * Add base mui layout with upload button
    
    * Add Biomejs and configure to formatt on save
    
    * Basic UI with upload button
    
    * Upload UX with image compression for Gemini

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{
+          color: "#FFF",
+          backgroundColor: "#111"
+      }}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {children}
