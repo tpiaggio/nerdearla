0853a98fb665ac3143178cf6e372092887eb13c2
gartz <gartz@google.com>
2024-09-05 23:14:34 +0000

    Add genkit integration

--- app/uploadContainer.tsx
+++ app/uploadContainer.tsx
@@ -3,8 +3,33 @@
 import { Container, Box } from "@mui/material";
 import Upload, { type onReadyProps } from "./component/upload";
 import { useState, useCallback } from "react";
+import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
 
-const fakeApiCall = async (_data: onReadyProps): Promise<string> => {
+const app = initializeApp({
+  apiKey: "",
+  authDomain: "nerdearla-workshop.firebaseapp.com",
+  projectId: "nerdearla-workshop",
+  storageBucket: "nerdearla-workshop.appspot.com",
+  messagingSenderId: "693219596825",
+  appId: "1:693219596825:web:70b9361c200af6160ee888",
+});
+const functions = getFunctions(app);
+connectFunctionsEmulator(
+  functions,
+  "3100-idx-nerdearla-1724776699374.cluster-t23zgfo255e32uuvburngnfnn4.cloudworkstations.dev",
+  3100,
+);
+
+const imageAnalisysFlow = httpsCallable(functions, "imageAnalisysFlow");
+
+const apiCall = async (data: onReadyProps): Promise<string> => {
+  const { data: result } = await imageAnalisysFlow({
+    url: data.url,
+    contentType: data.contentType,
+  });
+
+  console.log(result);
   return "The Gemini text response goes here.";
 };
 
@@ -22,14 +47,17 @@ export default function InteractionContainer() {
 
   const onReadyToUpload = useCallback(async (data: onReadyProps) => {
     // TODO: Backend API call with data.
-    const text = await fakeApiCall(data);
+    const text = await apiCall(data);
     setGeminiResponse(text);
   }, []);
 
   return (
     <Container>
       <Upload onImgUrlChange={onUpdateImgUrl} onReadyToUpload={onReadyToUpload} />
-      {imgSrc && <img src={imgSrc} alt="User uploaded" />}
+      {
+        // eslint-disable-next-line @next/next/no-img-element
+        imgSrc && <img src={imgSrc} alt="User uploaded" />
+      }
       <Box>{geminiResponse}</Box>
     </Container>
   );
